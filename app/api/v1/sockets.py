3748c5a511a5ce7f3be47095e4d6d2af2c006e93
Stepan <bolshakov.ca@gmail.com>
2025-12-05 21:43:11 +0700

    Fixed sockets service in endpoints

--- app/api/v1/sockets.py
+++ app/api/v1/sockets.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, WebSocket
 
 from api.dependencies.context import UserIdDep
-from api.dependencies.services import UsersServiceDep
+from api.dependencies.services import UsersServiceDep, SocketsServiceDep
 
 router = APIRouter(prefix="/ws", tags=["websockets"])
 
@@ -9,9 +9,9 @@ router = APIRouter(prefix="/ws", tags=["websockets"])
 @router.post("/")
 async def get_token(
     user_oid: UserIdDep,
-    users_service: UsersServiceDep,
+    sockets_service: SocketsServiceDep,
 ):
-    data = await users_service.get_token(user_oid=user_oid)
+    data = await sockets_service.get_token(user_oid=user_oid)
     return {
         "status": "success",
         "data": data,
@@ -22,10 +22,10 @@ async def get_token(
 async def ws_connect(
     token: str,
     websocket: WebSocket,
-    users_service: UsersServiceDep,
+    sockets_service: SocketsServiceDep,
 ):
     try:
-        await users_service.ws_connect(
+        await sockets_service.ws_connect(
             websocket=websocket,
             token=token,
         )
